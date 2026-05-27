2d1215c4b45b7f16fa7198c433186f1843fa6988
Xguille04 <guilleadr04@gmail.com>
2026-05-27 19:13:01 +0200

    fix: update CORS configuration to allow PATCH method

--- backend-spring/src/main/java/com/albalatefs/backend/security/WebSecurityConfig.java
+++ backend-spring/src/main/java/com/albalatefs/backend/security/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
